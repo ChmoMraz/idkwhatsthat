bc091243c7a208673a4526e2ea03130e769b8880
ChmoMraz <mr.sokoloff@inbox.ru>
2024-02-06 18:43:54 +0700

    int a equal to 4

--- idk/src/Main.java
+++ idk/src/Main.java
@@ -1,6 +1,8 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int a;
+        int a = 0;
+        a += 4;
+        System.out.println(a);
     }
 }
